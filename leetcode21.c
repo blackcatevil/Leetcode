daee63077f8fd1f41488d26f164cee585193cb69
Howard Chiu <howard.chiu@quantatw.com>
2018-10-19 15:29:03 +0800

    Title: Merge Two Sorted Lists
    Approach:
    	Using divide-and-conquer strategy to improve the time complexity.

--- leetcode21.c
+++ leetcode21.c
@@ -3,55 +3,45 @@ struct ListNode {
   struct ListNode *next;
 };
 
-struct ListNode* insertionSortList(struct ListNode* head)
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
 {
-  struct ListNode *p1, *p2, *tmp, *current, *tier, *front;
+  struct ListNode *p = NULL;
+  struct ListNode *head = NULL;
 
-  if (head == NULL)
-    return NULL;
+  if (l1 == NULL)
+    return l2;
+  if (l2 == NULL)
+    return l1;
 
-  current = head->next;
-  tier = head;
-  front = head;
-  while (current != NULL) {
-      p1 = NULL;
-      p2 = front;
-      while (p2 != current) {
-            if (p2->val > current->val) {
-	           tmp = current->next;
-	    
-	           if (p1 != NULL)
-	             p1->next = current;
-	           else
-	             front = current;
-	           current->next = p2;
-	           tier->next = tmp;
-	           current = tmp;
-	           break;
-	          }
-            else {
-	           p1 = p2;
-	           p2 = p2->next;
-	          }
-          }
-      if (p2 == current) {
-            tier = current;
-            current = current->next;
-          }
+  do {
+    if (l1->val > l2->val) {
+      if (p != NULL)
+	p->next = l2;
+      else
+	head = l2;
+
+      p = l2;
+      l2 = l2->next;
+    } else {
+      if (p != NULL)
+	p->next = l1;
+      else
+	head = l1;
+
+      p = l1;
+      l1 = l1->next;
     }
+  } while (l1 != NULL && l2 != NULL);
 
-  return front;
+  if (l1 == NULL)
+    p->next = l2;
+  else
+    p->next = l1;
+
+  return head;
 }
 
-struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
-    struct ListNode *p = l1;
-
-    if (p == NULL)
-        return insertionSortList(l2);
-
-    while(p->next != NULL)
-        p = p->next;
-    p->next = l2;
-
-    return insertionSortList(l1);
+int main()
+{
+  return 0;
 }
