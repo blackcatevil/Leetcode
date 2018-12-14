30a9a9e830572be9ec125c8bec0281a08685a612
Howard Chiu <howard.chiu@quantatw.com>
2018-12-14 11:00:49 +0800

    Title: Lowest Common Ancestor of a Binary Tree (Improved)
    Approach:
    	Find the target node in both left/right subtree recursively
    	then return its pointer.
    	If both target nodes are in the same subtree, the subtree
    	will only return the node which is in the higher level.
    
    	Since we allows a node to be a descendant of itself,
    	this node will be the LCA.

--- leetcode236.c
+++ leetcode236.c
@@ -7,50 +7,30 @@ struct TreeNode {
   struct TreeNode *right;
 };
 
-int findNode(struct TreeNode* root, struct TreeNode* target, struct TreeNode*** stack, int* size)
-{
-  if (root == NULL)
-    return 0;
-
-  (*stack) = (struct TreeNode**)realloc(*stack, sizeof(struct TreeNode*)*((*size)+1));
-  (*stack)[(*size)++] = root;
-  if (root == target ||
-      findNode(root->left, target, stack, size) > 0 ||
-      findNode(root->right, target, stack, size) > 0)
-    return 1;
-
-  (*stack) = (struct TreeNode**)realloc(*stack, sizeof(struct TreeNode*)*((*size)-1));
-  (*size)--;
-  return 0;
-}
-
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q)
 {
-  struct TreeNode** pstack = malloc(sizeof(struct TreeNode*));
-  struct TreeNode** qstack = malloc(sizeof(struct TreeNode*));
-  struct TreeNode* ans;
-  int psize = 0;
-  int qsize = 0;
+  struct TreeNode* right;
+  struct TreeNode* left;
 
   if (root == NULL)
     return NULL;
 
-  findNode(root, p, &pstack, &psize);
-  findNode(root, q, &qstack, &qsize);
-  if (psize > qsize)
-    psize = qsize;
+  if (root == p)
+    return p;
+  if (root == q)
+    return q;
 
-  for (int i = 1; i < psize; i++) {
-    if (pstack[i] != qstack[i]) {
-      ans = pstack[i-1];
-      break;
-    }
-    ans = pstack[i];
-  }
-  free(pstack);
-  free(qstack);
+  left = lowestCommonAncestor(root->left, p, q);
+  right = lowestCommonAncestor(root->right, p, q);
 
-  return ans;
+  if (left != NULL && right != NULL)
+    return root;
+  else if (left != NULL)
+    return left;
+  else if (right != NULL)
+    return right;
+  else
+    return NULL;
 }
 
 int main()
